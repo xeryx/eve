56dac92510b100192728a0665f0444a3e16e0054
Alfredo Moreno <moreno.a@gmail.com>
2017-10-17 16:35:28 -0400

    Added notes, clean express app.js

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var http = require('http');
 require('./models/models');
 
 mongoose.connect("mongodb://localhost:27017/agentstest", { useMongoClient: true });
@@ -19,17 +19,11 @@ var default_router = function(req, res)   {
 
 var app = express(); 
 
-// view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'ejs');
-
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 //app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', apihome);
 app.use('/agents', apiagents);
@@ -55,7 +49,6 @@ if (app.get('env') === 'development') {
         });
     });
 }
-
 
 module.exports = app;
 
